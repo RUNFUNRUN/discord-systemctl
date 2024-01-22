440a0f368b567cb8ee847f862a69d8ae40f27cec
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-01-23 00:11:58 +0900

    log追加

--- src/commands.ts
+++ src/commands.ts
@@ -6,6 +6,8 @@ export const ping: Command = {
   data: new SlashCommandBuilder().setName("ping").setDescription("test command"),
   execute: async (interaction) => {
     await interaction.reply("pong!");
+    console.log("/ping called");
+
   }
 };
 
@@ -13,8 +15,11 @@ export const start: Command = {
   data: new SlashCommandBuilder().setName("start").setDescription("start systemd"),
   execute: async (interaction) => {
     await interaction.reply("起動してます...");
+    console.log("/start called");
     await $`sudo systemctl start ${process.env.SYSTEMD_NAME}`;
     await interaction.editReply("起動しました!");
+    console.log("systemd started");
+
   }
 };
 
@@ -22,8 +27,10 @@ export const stop: Command = {
   data: new SlashCommandBuilder().setName("stop").setDescription("stop systemd"),
   execute: async (interaction) => {
     await interaction.reply("停止してます...");
+    console.log("/stop called");
     await $`sudo systemctl stop ${process.env.SYSTEMD_NAME}`;
     await interaction.editReply("停止しました!");
+    console.log("systemd stopped");
   }
 };
 
@@ -31,8 +38,10 @@ export const restart: Command = {
   data: new SlashCommandBuilder().setName("restart").setDescription("restart systemd"),
   execute: async (interaction) => {
     await interaction.reply("再起動してます...");
+    console.log("/restart called");
     await $`sudo systemctl restart ${process.env.SYSTEMD_NAME}`;
     await interaction.editReply("再起動しました!");
+    console.log("systemd restarted");
   }
 };
 
@@ -40,8 +49,10 @@ export const status: Command = {
   data: new SlashCommandBuilder().setName("status").setDescription("status systemd"),
   execute: async (interaction) => {
     await interaction.reply("状態を確認してます...");
+    console.log("/status called");
     const result = await $`sudo systemctl status ${process.env.SYSTEMD_NAME}`.text();
     await interaction.editReply(`\`\`\`\n${result}\n\`\`\``);
+    console.log("systemd status");
   }
 };
 
